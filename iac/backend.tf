ca52e8d20961a0ade4d4e739d63f1791393ebf6b
asxkishor <45590788+asxkishor@users.noreply.github.com>
2023-05-27 02:47:15 +0530

    Update backend.tf

--- iac/backend.tf
+++ iac/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    # modify the values for the storage account
+    # modify the values for the storage account - added the details
     storage_account_name = "teststorage27052023"
     container_name       = "testingterraform"
     key                  = "terraform.tfstate"
